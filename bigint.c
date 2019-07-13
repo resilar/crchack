0888add94220c05c760d0c67c72293bc34e8d2bb
def <def@huumeet.info>
2019-07-13 23:15:02 +0300

    rewrite the shitty bigint library

--- bigint.c
+++ bigint.c
@@ -1,99 +1,53 @@
 #include "bigint.h"
-#include <string.h>
 
 void bigint_fprint(FILE *stream, const struct bigint *dest)
 {
-    size_t i;
-
-    /* Iterate nibbles. The most significant nibble first */
-    i = (dest->bits-1) / 4;
-    do {
-        unsigned char nibble;
-
-        /* (WORD_BIT/4) nibbles per word */
-        nibble = dest->buf[i / (WORD_BIT/4)] >> ((i % (WORD_BIT/4)) * 4);
-        nibble &= 0x0F;
-
-        if (nibble <= 0x09) {
-            fputc(nibble + '0', stream);
-        } else fputc(nibble - 0x0A + 'a', stream);
-
-    } while (i--);
-}
-
-int bigint_from_string(struct bigint *dest, const char *hex_string)
-{
-    size_t length, bits, i, read;
-
-    /* Skip 0x prefix */
-    if (hex_string[0] == '0' && hex_string[1] == 'x')
-        hex_string += 2;
-
-    /* Check format */
-    length = strspn(hex_string, "0123456789abcdefABCDEF");
-    if (length == 0 || hex_string[length] != '\0') {
-        /* Not a valid hexadecimal value */
-        return 0;
-    }
-
-    /* Calculate length in bits (exclude leading zeros) */
-    for (i = 0; i < length-1 && hex_string[i] == '0'; i++);
-    bits = (length-i)*4;
-    bits -= (hex_string[i] < '8') + (hex_string[i] < '4') /* trollface.jpg */
-          + (hex_string[i] < '2') + (hex_string[i] < '1');
-    if (bits > dest->bits) return 0; /* Too big */
-
-    /* Convert */
-    bigint_load_zeros(dest);
-    for (read = 0; read < bits; read += 4) {
-        char c = hex_string[length-1 - read/4];
-        if (c >= '0' && c <= '9') {
-            dest->buf[read/WORD_BIT] |= (c - '0') << (read % WORD_BIT);
-        } else if (c >= 'A' && c <= 'F') {
-            dest->buf[read/WORD_BIT] |= (10 + (c - 'A')) << (read % WORD_BIT);
-        } else /*if (c >= 'a' && c <= 'f')*/ {
-            dest->buf[read/WORD_BIT] |= (10 + (c - 'a')) << (read % WORD_BIT);
+    size_t i, j, k = (LIMB_BITS/4) - ((dest->bits + 3) % LIMB_BITS)/4;
+    for (i = 1, j = bigint_limbs(dest); i <= j; i++) {
+        const limb_t limb = dest->buf[j-i];
+        for (k = k % (LIMB_BITS/4); k < LIMB_BITS/4; k++) {
+            unsigned char nibble = (limb >> (LIMB_BITS - 4*(k+1))) & 0x0F;
+            fputc((nibble < 10) ? (nibble + '0') : (nibble - 10 + 'a'), stdout);
         }
     }
-
-    return 1;
 }
 
-void bigint_shl_1(struct bigint *dest)
+struct bigint *bigint_from_string(struct bigint *dest, const char *hex)
 {
-    size_t i;
+    size_t len, bits, i;
 
-    /* The highest word */
-    i = bigint_words(dest)-1;
-    dest->buf[i] <<= 1;
-    if (dest->bits % WORD_BIT)
-        dest->buf[i] &= ((word)1 << (dest->bits % WORD_BIT)) - 1;
+    /* Validate input string */
+    if (hex[0] == '0' && hex[1] == 'x')
+        hex += 2;
+    len = strspn(hex, "0123456789abcdefABCDEF");
+    if (!len || hex[len] != '\0')
+        return NULL;
 
-    /* The rest */
-    while (i > 0) {
-        i--;
-        dest->buf[i+1] |= dest->buf[i] >> (WORD_BIT-1);
-        dest->buf[i] <<= 1;
+    /* Determine length in bits */
+    for (i = 0; i < len-1 && hex[i] == '0'; i++);
+    bits = (len-i)*4;
+    bits -= (hex[i] < '8') + (hex[i] < '4') /* trollface.jpg */
+          + (hex[i] < '2') + (hex[i] < '1');
+    if (bits > dest->bits)
+        return NULL; /* need moar space */
+
+    /* Parse into limbs */
+    bigint_load_zeros(dest);
+    for (i = 0; i < bits; i += 4) {
+        char v, c = hex[len-1 - i/4];
+        if (c >= '0' && c <= '9') {
+            v = c - '0';
+        } else if (c >= 'A' && c <= 'F') {
+            v = 10 + (c - 'A');
+        } else /*if (c >= 'a' && c <= 'f')*/ {
+            v = 10 + (c - 'a');
+        }
+        dest->buf[i/LIMB_BITS] |= (limb_t)v << (i % LIMB_BITS);
     }
+    return dest;
 }
 
-void bigint_shr_1(struct bigint *dest)
-{
-    size_t i;
-
-    /* The first word */
-    i = 0;
-    dest->buf[i] >>= 1;
-
-    /* The rest */
-    while (i < bigint_words(dest)-1) {
-        i++;
-        dest->buf[i-1] |= (word)(dest->buf[i] & 1) << (WORD_BIT-1);
-        dest->buf[i] >>= 1;
-    }
-}
-
-void bigint_reflect(struct bigint *dest)
+struct bigint *bigint_reflect(struct bigint *dest)
 {
     struct bigint acc;
     if (bigint_init(&acc, dest->bits)) {
@@ -108,12 +62,13 @@ void bigint_reflect(struct bigint *dest)
         }
         bigint_destroy(&acc);
     }
+    return dest;
 }
 
+/* snorlax edition */
 size_t bigint_popcount(struct bigint *dest)
 {
-    size_t ret, i;
-    ret = 0;
+    size_t i, ret = 0;
     for (i = 0; i < dest->bits; i++) {
         if (bigint_get_bit(dest, i))
             ret++;
--- bigint.h
+++ bigint.h
@@ -1,84 +1,91 @@
 /**
- * Big integers for crchack.
+ * Rudimentary big integers for crchack.
  */
 #ifndef BIGINT_H
 #define BIGINT_H
 
 #include <assert.h>
 #include <memory.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef uint32_t word;
-#define WORD_BIT (sizeof(word)*8)
-
+typedef unsigned long limb_t;
+#define LIMB_BITS (sizeof(limb_t)*8)
 struct bigint {
-    word *buf;
-    size_t bits; /* size */
+    limb_t *buf;
+    size_t bits;
 };
 
-/**
- * Bigint size in bytes and words.
- */
+/* Number of bigint limbs */
+static inline size_t bigint_limbs(const struct bigint *dest)
+{
+    return dest->bits ? (1 + (dest->bits - 1) / LIMB_BITS) : 0;
+}
+
+/* Size of bigint in bytes */
 static inline size_t bigint_sizeof(const struct bigint *dest)
 {
-    return 1 + (dest->bits-1)/8; /* ceil() */
+    return bigint_limbs(dest) * sizeof(limb_t);
 }
 
-static inline size_t bigint_words(const struct bigint *dest)
-{
-    return 1 + (dest->bits-1)/WORD_BIT;
-}
-
-/**
- * (De)initialize a bigint structure.
- */
+/* Initialize w-bit bigint structure */
 static inline struct bigint *bigint_init(struct bigint *dest, size_t bits)
 {
-    dest->bits = bits;
-    dest->buf = (bits) ? calloc(bigint_words(dest), sizeof(word)) : NULL;
-    return (dest->buf) ? dest : NULL;
+    if ((dest->bits = bits)) {
+        if ((dest->buf = calloc(bigint_limbs(dest), sizeof(limb_t))))
+            return dest;
+    }
+    dest->bits = 0;
+    dest->buf = NULL;
+    return NULL;
 }
 
+/* Initialize bigint and its value from an existing bigint.  */
+static inline struct bigint *bigint_init_from(struct bigint *dest,
+                                              const struct bigint *from)
+{
+    if ((dest->bits = from->bits)) {
+        size_t size = bigint_sizeof(from);
+        if ((dest->buf = malloc(size))) {
+            memcpy(dest->buf, from->buf, size);
+            return dest;
+        }
+    }
+    dest->bits = 0;
+    dest->buf = 0;
+    return NULL;
+}
+
+/* Destroy bigint (deallocate dynamic memory) */
 static inline void bigint_destroy(struct bigint *dest)
 {
     if (dest) {
         free(dest->buf);
         dest->buf = NULL;
+        dest->bits = 0;
     }
 }
 
-/**
- * Functions declared below expect the passed in bigint structures to be
- * properly initialized.
- */
-
-/**
- * Print bigint as a hex value to the stream.
- */
+/* Print hexadecimal representation of a bigint to stream */
 void bigint_fprint(FILE *stream, const struct bigint *dest);
 #define bigint_print(x) (bigint_fprint(stdout, (x)))
 
-/**
- * Initialize all bits of a bigint to 0/1.
- */
+/* Set all bits to zero */
 static inline void bigint_load_zeros(struct bigint *dest)
 {
     memset(dest->buf, 0, bigint_sizeof(dest));
 }
 
+/* Set all bits to one */
 static inline void bigint_load_ones(struct bigint *dest)
 {
     memset(dest->buf, -1, bigint_sizeof(dest));
 }
 
-/**
- * Check whether bigint is zero.
- */
+/* Test for zero value */
 static inline int bigint_is_zero(const struct bigint *dest)
 {
-    size_t i, j = bigint_words(dest);
+    size_t i, j = bigint_limbs(dest);
     for (i = 0; i < j; i++) {
         if (dest->buf[i])
             return 0;
@@ -87,111 +94,121 @@ static inline int bigint_is_zero(const struct bigint *dest)
 }
 
 /**
- * Load bigint value from an ASCII representation of a hexadecimal value.
+ * Load bigint from a hex string.
  *
- * The function fails if the passed in string does not represent a valid
- * hexadecimal value, or if the destination bigint is too small to hold the
- * value.
+ * Fails if the input string is an invalid hexadecimal number, or if destination
+ * bigint is too small to hold the value. Returns dest (or NULL on failure).
  */
-int bigint_from_string(struct bigint *dest, const char *hex_string);
+struct bigint *bigint_from_string(struct bigint *dest, const char *hex);
 
-/**
- * Get/set the value of the nth least significant bit (n = 0, 1, 2, ...)
- */
+/* Get/set the nth least significant bit (n = 0, 1, 2, ..., dest->bits - 1) */
 static inline int bigint_get_bit(const struct bigint *dest, size_t n)
 {
-    return !!(dest->buf[n / WORD_BIT] & ((word)1 << (n % WORD_BIT)));
+    return (dest->buf[n / LIMB_BITS] >> (n % LIMB_BITS)) & 1;
 }
 #define bigint_lsb(x) ((x)->buf[0] & 1)
 #define bigint_msb(x) (bigint_get_bit((x), (x)->bits-1))
 
 static inline void bigint_set_bit(const struct bigint *dest, size_t n)
 {
-    dest->buf[n / WORD_BIT] |= ((word)1 << (n % WORD_BIT));
+    dest->buf[n / LIMB_BITS] |= ((limb_t)1 << (n % LIMB_BITS));
 }
 #define bigint_set_lsb(x) ((x)->buf[0] |= 1)
 #define bigint_set_msb(x) (bigint_set_bit((x), (x)->bits-1))
 
 static inline void bigint_clear_bit(const struct bigint *dest, size_t n)
 {
-    dest->buf[n / WORD_BIT] &= ~((word)1 << (n % WORD_BIT));
+    dest->buf[n / LIMB_BITS] &= ~((limb_t)1 << (n % LIMB_BITS));
 }
-#define bigint_clear_lsb(x) ((x)->buf[0] &= ~(word)1)
+#define bigint_clear_lsb(x) ((x)->buf[0] &= ~(limb_t)1)
 #define bigint_clear_msb(x) (bigint_clear_bit((x), (x)->bits-1))
 
 static inline void bigint_flip_bit(const struct bigint *dest, size_t n)
 {
-    dest->buf[n / WORD_BIT] ^= ((word)1 << (n % WORD_BIT));
+    dest->buf[n / LIMB_BITS] ^= ((limb_t)1 << (n % LIMB_BITS));
 }
 #define bigint_flip_lsb(x) ((x)->buf[0] ^= 1)
 #define bigint_flip_msb(x) (bigint_flip_bit((x), (x)->bits-1))
 
-/**
- * Move (copy) the source value to the destination.
- */
-static inline void bigint_mov(struct bigint *dest, const struct bigint *src)
+/* Move (copy) source bigint to destination */
+static inline struct bigint *bigint_mov(struct bigint *dest,
+                                        const struct bigint *src)
 {
     assert(dest->bits == src->bits);
     memcpy(dest->buf, src->buf, bigint_sizeof(dest));
+    return dest;
 }
 
-/**
- * Bitwise NOT operation.
- */
-static inline void bigint_not(struct bigint *dest)
+/* Bitwise NOT */
+static inline struct bigint *bigint_not(struct bigint *dest)
 {
-    size_t i, j = bigint_words(dest);
+    size_t i, j = bigint_limbs(dest);
     for (i = 0; i < j; i++)
         dest->buf[i] = ~dest->buf[i];
+    return dest;
 }
 
-/**
- * Exclusive-OR.
- */
-static inline void bigint_xor(struct bigint *dest, const struct bigint *src)
+/* Bitwise XOR */
+static inline struct bigint *bigint_xor(struct bigint *dest,
+                                        const struct bigint *src)
 {
-    size_t i, j = bigint_words(dest);
+    size_t i, j;
     assert(dest->bits == src->bits);
-    for (i = 0; i < j; i++)
+    for (i = 0, j = bigint_limbs(dest); i < j; i++)
         dest->buf[i] ^= src->buf[i];
+    return dest;
 }
 
-/**
- * Bitwise AND.
- */
-static inline void bigint_and(struct bigint *dest, const struct bigint *src)
+/* Bitwise AND */
+static inline struct bigint *bigint_and(struct bigint *dest,
+                                        const struct bigint *src)
 {
-    size_t i, j = bigint_words(dest);
+    size_t i, j = bigint_limbs(dest);
     assert(dest->bits == src->bits);
     for (i = 0; i < j; i++)
         dest->buf[i] &= src->buf[i];
+    return dest;
 }
 
-/**
- * Swap the values of two bigints.
- */
+/* Swap the values of two bigints */
 static inline void bigint_swap(struct bigint *a, struct bigint *b) {
-    word *buf;
     assert(a->bits == b->bits);
-    buf = a->buf;
+    limb_t *buf = a->buf;
     a->buf = b->buf;
     b->buf = buf;
 }
 
-/**
- * Shift bigint value to the left or right by 1 bit.
- */
-void bigint_shl_1(struct bigint *dest);
-void bigint_shr_1(struct bigint *dest);
+/* Bit-shift to the left by 1 */
+static inline struct bigint *bigint_shl_1(struct bigint *dest)
+{
+    size_t i, j;
+    limb_t carry = 0;
+    for (i = 0, j = bigint_limbs(dest) - 1; i < j; i++) {
+        limb_t c = (dest->buf[i] >> (LIMB_BITS - 1)) & 1;
+        dest->buf[i] = (dest->buf[i] << 1) | carry;
+        carry = c;
+    }
+    dest->buf[i] &= ((limb_t)1 << ((dest->bits - 1) % LIMB_BITS)) - 1;
+    dest->buf[i] = (dest->buf[i] << 1) | carry;
+    return dest;
+}
 
-/**
- * Reverse the bits in bigint.
- */
-void bigint_reflect(struct bigint *dest);
+/* Bit-shift to the right by 1 */
+static inline struct bigint *bigint_shr_1(struct bigint *dest)
+{
+    size_t i, j;
+    for (i = 0, j = bigint_limbs(dest) - 1; i < j; i++) {
+        dest->buf[i] = (dest->buf[i] >> 1) | (dest->buf[i+1] << (LIMB_BITS-1));
+    }
+    dest->buf[i] >>= 1;
+    return dest;
+}
 
-/**
- * Population count (Hamming weight of bits).
- */
+
+/* Reverse the bits in a bigint (LSB becomes MSB and vice versa and so on) */
+struct bigint *bigint_reflect(struct bigint *dest);
+
+/* Population count (Hamming weight of bits, i.e., the number of ones) */
 size_t bigint_popcount(struct bigint *dest);
 
 #endif
